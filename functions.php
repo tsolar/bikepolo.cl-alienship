a6cf462d5e540972a06857a9e05639a6ffe6dd80
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-12-12 03:08:54 -0300

    el include no era necesario, y en el server es case-sensitive así que el screen_name se deja StgoBikePolo

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once '../alienship/functions.php';
-
 function get_bikepolo_tag_tweets()
 {
     $tweets_json = file_get_contents("https://search.twitter.com/search.json?q=bikepolo", true);
@@ -13,7 +11,7 @@ function get_bikepolo_tag_tweets()
 
 function get_stgobikepolo_tweets()
 {
-    $tweets_json = file_get_contents("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=stgobikepolo", true);
+    $tweets_json = file_get_contents("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=StgoBikePolo");
 
     $tweets = json_decode($tweets_json);
 
