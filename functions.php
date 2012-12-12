cae112c7a19868b2fa01bcab47d7850d89a2f900
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-12-11 21:09:00 -0300

    se agrega twitter StgoBikePolo

--- functions.php
+++ functions.php
@@ -2,13 +2,21 @@
 
 include_once '../alienship/functions.php';
 
-function get_tweets()
+function get_bikepolo_tag_tweets()
 {
-    $tweets_json = file_get_contents("https://search.twitter.com/search.json?q=bikepolo", true); //getting the file content
+    $tweets_json = file_get_contents("https://search.twitter.com/search.json?q=bikepolo", true);
 
     $tweets = json_decode($tweets_json);
 
     return $tweets;
 }
 
-?>
+function get_stgobikepolo_tweets()
+{
+    $tweets_json = file_get_contents("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=stgobikepolo", true);
+
+    $tweets = json_decode($tweets_json);
+
+    return $tweets;
+}
+
--- tweets.php
+++ tweets.php
@@ -2,15 +2,32 @@
     <h3>#bikepolo</h3>
     <ul>
         <?php
-        $tweets = get_tweets();
+        $tweets = get_bikepolo_tag_tweets();
         foreach ($tweets->results as $k=>$tweet)
         {
             if($k>=5) break;
             ?>
             <li>
-                <?php
-                    echo $tweet->text;
-                ?>
+                <span class="text"><?php echo $tweet->text; ?></span>
+            </li>
+            <?php
+        }
+        ?>
+    </ul>
+    <h3>
+        <a href="//twitter.com/StgoBikePolo" target="_blank">
+            @StgoBikePolo
+        </a>
+    </h3>
+    <ul>
+        <?php
+        $tweets = get_stgobikepolo_tweets();
+        foreach ($tweets as $k=>$tweet)
+        {
+            if($k>=5) break;
+            ?>
+            <li>
+                <span class="text"><?php echo $tweet->text; ?></span>
             </li>
             <?php
         }
