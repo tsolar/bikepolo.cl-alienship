0c3a6e854b117e23aabde8d7b5044925c026e9ce
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-12-12 02:23:47 -0300

    fondo_oscurito -> fondo-oscurito

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,7 @@
   <div id="sidebar" class="<?php echo apply_filters( 'alienship_sidebar_container_class', 'span3' ); ?>">
     <?php include_once 'tweets.php'; ?>
     <?php do_action( 'alienship_sidebar_inside_before' ); ?>
-    <div id="secondary" class="<?php echo apply_filters( 'alienship_secondary_container_class', 'widget-area well pull-left' ); ?>" role="complementary">
+    <div id="secondary" class="<?php echo apply_filters( 'alienship_secondary_container_class', 'widget-area fondo-oscurito pull-left' ); ?>" role="complementary">
       <?php do_action( 'before_sidebar' ); ?>
       <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
